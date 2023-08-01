15623603ee4651ade5ef03ca2d3851f9452e1495
mgrnz21 <mugiranezamagni@gmail.com>
2023-08-01 10:42:18 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,58 @@
 #include "main.h"
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+int findLength(char *s);
+int helper(char *s, int len, int a);
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
+ * is_palindrome - function with 1 argument
+ * @s: char type pointer argument
  *
- * Return: 1 if it is, 0 it's not
+ * Description: test if string is palindrome
+ * Return: 1 if palindrome or 0 if not
  */
 int is_palindrome(char *s)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (helper(s, findLength(s) - 1, 0));
 }
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
+ * findLength - function with 1 argument
+ * @s: char type pointer argument
  *
- * Return: length of the string
+ * Description: To find length of string
+ * Return: total index count of string
  */
-int _strlen_recursion(char *s)
+int findLength(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		return (findLength(s + 1) + 1);
+	}
+	else
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check * @i: iterator
- * @len: length of the string
+ * helper - helper function with 3 argument
+ * @s: char type pointer
+ * @len: int type argument
+ * @a: int type argument
  *
- * Return: 1 if palindrome, 0 if not
+ * Description: to test conditions with all inputs from other functions
+ * Return: 1 if palindrome 0 if false
  */
-int check_pal(char *s, int i, int len)
+int helper(char *s, int len, int a)
 {
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if (len ==  a)
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	else if (len - a == 1)
+	{
+		if (s[len] == s[a])
+			return (1);
+		else
+			return (0);
+	}
+	else if (s[len] != s[a])
+		return (0);
+	else
+		return (helper(s, --len, ++a));
 }
